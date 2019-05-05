7aca26fee8703503d3d0b910ae2fbb7b249c8af4
shemi <shemi@users.noreply.github.com>
2019-05-05 22:58:11 +0300

    Update generator.js

--- generator.js
+++ generator.js
@@ -39,36 +39,7 @@ module.exports = (api, options) => {
   })
 
   api.postProcessFiles(files => {
-    const hasTS = api.hasPlugin('typescript')
-
-    // router
-    if (api.hasPlugin('router')) {
-      let cordovaRouterMode = `process.env.CORDOVA_PLATFORM ? 'hash' : `
-      const routerFilePath = `src/router.${hasTS ? 'ts' : 'js'}`
-      const routerFile = files[routerFilePath]
-      if (routerFile) {
-        const lines = routerFile.split(/\r?\n/g).reverse()
-        const regex = /\s+mode:\s('|"?\w+'|"?)/
-        const modeIndex = lines.findIndex(line => line.match(regex))
-        if (modeIndex >= 0) {
-          const matches = lines[modeIndex].match(regex)
-          const routerMode = matches[1]
-          if (routerMode.includes('"')) {
-            cordovaRouterMode = cordovaRouterMode.replace(`'hash'`, `"hash"`)
-          }
-          const newRouterMode = cordovaRouterMode + routerMode
-          lines[modeIndex] = lines[modeIndex].replace(routerMode, newRouterMode)
-          api.exitLog('Updated ' + routerFilePath + ' : ' + newRouterMode)
-        } else {
-          if (routerFile.includes('mode:')) {
-            api.exitLog(`Unable to modify current router mode, make sure it's 'hash'`, 'warn')
-          }
-        }
-        files[routerFilePath] = lines.reverse().join('\n')
-      } else {
-        api.exitLog(`Unable to find router file, make sure router mode is 'hash'`, 'warn')
-      }
-    }
+    const hasTS = api.hasPlugin('typescript');
   })
 
   api.onCreateComplete(() => {
